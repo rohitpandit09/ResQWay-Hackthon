3e209c286d456d107b0b59cb5298f2fce97f2702
Rohit Pandit <rpandit0907@gmail.com>
2026-09-22 00:29:11 +0530

    Delete Backend/src/routes/locationRoutes.js

--- Backend/src/routes/locationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getClientLocation, getDriverLocation } = require('../utils/getLocation');
-const {clientMiddleware, driverMiddleware} = require('../middleware/authMiddleware');
-
-router.post('/client/:clientId/location', clientMiddleware, getClientLocation);
-router.post('/driver/:driverId/location', driverMiddleware, getDriverLocation);
-
-module.exports = router;
